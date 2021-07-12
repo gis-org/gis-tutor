414b7558f8a4a871dc601fb006023353e7dbb51c
yzqdev <yzqdev@outlook.com>
2021-07-12 16:35:27 +0800

    添加mapbox

--- src/view/Ol.vue
+++ src/view/Ol.vue
@@ -1,33 +1,175 @@
 <template>
-  <div></div>
+  <div>
+    <div id="viewDiv"></div>
+    <div id="editorDiv"></div>
+  </div>
 </template>
 
 <script>
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import XYZ from "ol/source/XYZ";
-
+import WebMap from "@arcgis/core/WebMap";
+import MapView from "@arcgis/core/views/MapView";
+import Editor from "@arcgis/core/widgets/Editor";
 export default {
   name: "Ol",
   mounted() {
-    new Map({
-      target: "map",
-      baseMap:"osm",
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-          }),
-        }),
-      ],
-      view: new View({
-        center: [0, 0],
-        zoom: 2,
-      }),
+
+    let pointLayer, lineLayer, polygonLayer;
+    const webmap = new WebMap({
+      basemap:'osm',
+      portalItem: {
+        id: "459a495fc16d4d4caa35e92e895694c8",
+      },
+    });
+
+    const view = new MapView({
+      container: "viewDiv",
+      map: webmap,
+      center:[117.13168780199648, 31.83669432640482]
+    });
+
+    view.when(() => {
+      view.map.loadAll().then(() => {
+        view.map.allLayers.forEach((layer) => {
+          if (layer.type === "feature") {
+            switch (layer.geometryType) {
+              case "polygon":
+                polygonLayer = layer;
+                break;
+              case "polyline":
+                lineLayer = layer;
+                break;
+              case "point":
+                pointLayer = layer;
+                break;
+            }
+          }
+        });
+
+        // Create layerInfos for layers in Editor. This
+        // sets the fields for editing.
+
+        const pointInfos = {
+          layer: pointLayer,
+          fieldConfig: [
+            {
+              name: "HazardType",
+              label: "头部数据",
+            },
+            {
+              name: "Description",
+              label: "描述",
+            },
+            {
+              name: "SpecialInstructions",
+              label: "特殊的介绍",
+            },
+            {
+              name: "Status",
+              label: "Status",
+            },
+            {
+              name: "Priority",
+              label: "Priority",
+            },
+          ],
+        };
+
+        const lineInfos = {
+          layer: lineLayer,
+          fieldConfig: [
+            {
+              name: "Severity",
+              label: "Severity",
+            },
+            {
+              name: "blocktype",
+              label: "Type of blockage",
+            },
+            {
+              name: "fullclose",
+              label: "Full closure",
+            },
+            {
+              name: "active",
+              label: "Active",
+            },
+            {
+              name: "locdesc",
+              label: "地点描述",
+            },
+          ],
+        };
+
+        const polyInfos = {
+          layer: polygonLayer,
+          fieldConfig: [
+            {
+              name: "incidenttype",
+              label: "Incident Type",
+            },
+            {
+              name: "activeincid",
+              label: "Active",
+            },
+            {
+              name: "descrip",
+              label: "特殊介绍",
+            },
+          ],
+        };
+
+        const editor = new Editor({
+          view: view,
+          layerInfos: [
+            {
+              layer: pointLayer,
+              fieldConfig: [pointInfos],
+            },
+            {
+              layer: lineLayer,
+              fieldConfig: [lineInfos],
+            },
+            {
+              layer: polygonLayer,
+              fieldConfig: [polyInfos],
+            },
+          ],
+          // Set the snapping options for the Editor. By default, snapping is enabled. This can be toggled on/off using the CTRL key.
+          snappingOptions: {
+            enabled: true,
+            selfEnabled: true,
+            featureEnabled: true,
+            featureSources: [
+              {
+                layer: pointLayer,
+              },
+              {
+                layer: lineLayer,
+              },
+              {
+                layer: polygonLayer,
+              },
+            ],
+          },
+        });
+        // Add widget to top-right of the view
+        view.ui.add(editor, "top-right");
+      });
     });
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import "https://js.arcgis.com/4.20/esri/themes/light/main.css";
+#viewDiv {
+  padding: 0;
+  margin: 0;
+  height: 100vh;
+  width: 100%;
+}
+
+.esri-editor .esri-item-list__scroller {
+  max-height: 350px;
+}
+</style>
--- src/view/SimpleMap.vue
+++ src/view/SimpleMap.vue
@@ -1,38 +1,64 @@
 <template>
-<div class="simple-map"></div>
+  <div class="container">
+    <div class="simple-map"></div>
+  </div>
 </template>
 
 <script>
+import esriConfig from "@arcgis/core/config";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
-import Expand from "@arcgis/core/widgets/Expand";
-import * as intl from "@arcgis/core/intl";
+import Search from '@arcgis/core/widgets/Search'
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 export default {
   name: "SimpleMap",
   async mounted() {
-    intl.setLocale("zh-CN")
+    //部分功能可能需要收费,请酌情考虑
+    esriConfig.apiKey =
+      "AAPKa9c42d14dcab4807b6eaf91a8471e1b5bGFEmnQxkgCpw_lZoRgNjSwS-ub3AO0FuQaDCXOtMfmDVHi65mJGVCTdR5GRAKnw";
+
+
     const map = new Map({
-      basemap: "osm"
+      basemap: "arcgis-topographic" //Basemap layer service
     });
 
     const view = new MapView({
       container: this.$el,
       map: map,
-      zoom: 4,
-      center: [117.13168780199648, 31.83669432640482] // longitude, latitude
+      center: [-118.80543,34.02700], //Longitude, latitude
+      zoom: 13
     });
-    // view.ui.add(map, "top-right");
-  }
-}
+
+
+    const popupTrailheads = {
+      "title": '标题',
+      "content": "<b>小路:</b> {TRL_NAME}<br><b>城市:</b> {CITY_JUR}<br><b>十字路:</b> {X_STREET}<br><b>Parking:</b> {PARKING}<br><b>Elevation:</b> {ELEV_FT} ft"
+    }
+
+    const trailheads = new FeatureLayer({
+      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0",
+      outFields: ["TRL_NAME","CITY_JUR","X_STREET","PARKING","ELEV_FT"],
+      popupTemplate: popupTrailheads
+    });
+
+    map.add(trailheads);
+
+
+  },
+};
 </script>
 
 <style scoped>
-@import 'https://cdn.jsdelivr.net/npm/@arcgis/core/assets/esri/css/main.css';
-.simple-map{
+@import "https://cdn.jsdelivr.net/npm/@arcgis/core/assets/esri/css/main.css";
+.container {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+}
+.simple-map {
   padding: 0;
   margin: 0;
   width: 100%;
   height: 100vh;
-
 }
 </style>
